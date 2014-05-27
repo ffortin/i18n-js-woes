65c3516e02f6121a9a21a3b2c2a6c6b3d5039125
Francois Fortin <francois.fortin@adgear.com>
2014-05-27 09:11:55 -0400

    Fixes namespaces

--- lib/i18n/js/sprockets_preprocessor.rb
+++ lib/i18n/js/sprockets_preprocessor.rb
@@ -11,19 +11,21 @@ class Sprockets::DirectiveProcessor
   end
 end
 
-module SimplesIdeias
-  module I18n
-    class Preprocessor < ::Sprockets::Processor
-      def evaluate(context, locals)
-        if context.logical_path == "i18n/filtered"
-          config = I18n.config_file
+module I18n
+  module JS
+    module Engine
+      class Preprocessor < ::Sprockets::Processor
+        def evaluate(context, locals)
+          if context.logical_path == "i18n/filtered"
+            config = I18n.config_file
 
-          context.depend_on(config) if I18n.config?
-          # also set up dependencies on every locale file
-          ::I18n.load_path.each {|path| context.depend_on(path)}
+            context.depend_on(config) if I18n.config?
+            # also set up dependencies on every locale file
+            ::I18n.load_path.each {|path| context.depend_on(path)}
+          end
+
+          data
         end
-
-        data
       end
     end
   end
