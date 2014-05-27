f0f09d6959cbb41fdadd6273e346a689f414d878
Francois Fortin <francois.fortin@adgear.com>
2014-05-27 09:13:22 -0400

    wtf

--- lib/i18n/js/engine.rb
+++ lib/i18n/js/engine.rb
@@ -2,10 +2,25 @@ require "i18n/js"
 
 module I18n
   module JS
+
     class Engine < ::Rails::Engine
       initializer "i18n-js.asset_dependencies", :after => "sprockets.environment",
                                                 :before => "i18n-js.initialize" do
-        Rails.application.assets.register_preprocessor "application/javascript", Preprocessor
+        Rails.application.assets.register_preprocessor "application/javascript", ::I18n::JS::Preprocessor
+      end
+    end
+
+    class Preprocessor < ::Sprockets::Processor
+      def evaluate(context, locals)
+        if context.logical_path == "i18n/filtered"
+          config = I18n.config_file
+
+          context.depend_on(config) if I18n.config?
+          # also set up dependencies on every locale file
+          ::I18n.load_path.each {|path| context.depend_on(path)}
+        end
+
+        data
       end
     end
   end
