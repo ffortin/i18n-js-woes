6d82d1a03d2c4c0788a96cbe601b16411aad779d
Francois Fortin <francois.fortin@adgear.com>
2014-05-27 08:55:22 -0400

    Another attempt

--- lib/i18n/js/sprockets_preprocessor.rb
+++ lib/i18n/js/sprockets_preprocessor.rb
@@ -10,3 +10,21 @@ class Sprockets::DirectiveProcessor
     end
   end
 end
+
+module SimplesIdeias
+  module I18n
+    class Preprocessor < ::Sprockets::Processor
+      def evaluate(context, locals)
+        if context.logical_path == "i18n/filtered"
+          config = I18n.config_file
+
+          context.depend_on(config) if I18n.config?
+          # also set up dependencies on every locale file
+          ::I18n.load_path.each {|path| context.depend_on(path)}
+        end
+
+        data
+      end
+    end
+  end
+end
